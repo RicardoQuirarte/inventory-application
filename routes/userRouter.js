3c4c4f18ad2eb1f095f348873f4b634c7f43ed49
Ricardo Quirarte <reynaldo.quirarte@gmail.com>
2024-11-23 23:53:58 -0600

    correcting the http methods

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,7 +10,7 @@ usersRouter.get("/patient", usersController.searchPatient);
 usersRouter.get("/search", usersController.showPatient);
 usersRouter.get("/serchbyname", usersController.searchByName);
 usersRouter.get("/update", usersController.updatePatient);
-usersRouter.post("/update", usersController.updatePatientPost);
-usersRouter.post("/delete", usersController.deletePatient);
+usersRouter.put("/update", usersController.updatePatientPost);
+usersRouter.delete("/delete", usersController.deletePatient);
 
 module.exports = usersRouter;
